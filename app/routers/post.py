db8c16057604af5515a3d14c669e53f781910600
Michael Nana Ofosu <122281966+MNO-proA@users.noreply.github.com>
2023-08-04 12:08:52 +0000

    pydantic migration from v1 to v2

--- app/routers/post.py
+++ app/routers/post.py
@@ -21,11 +21,11 @@ router = APIRouter(
 #CREATE
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model= schemas.Post)
 def create_post(post : schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-  # cursor.execute(""" INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING * """,(post.title, post.content, post.published)) #Parameterized Query or Sanitized Query(Query Parameterization) =  against SQL Injections
+  # cursor.execute(""" INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING * """,(post.title, post.content, post.published)) 
+  # Parameterized Query or Sanitized Query(Query Parameterization) =  against SQL Injections
   # new_post = cursor.fetchone()
   # conn.commit()
 
-  """SQLALCHEMY"""
   # new_post = models.Post(title=post.title, content=post.content, published=post.published) Key=value <= **dict()
   new_post = models.Post(owner_id=current_user.id, **post.dict())
   db.add(new_post)
